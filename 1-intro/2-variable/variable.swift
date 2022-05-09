fbab08db54cfb321507d0448e6e4ef095bebae03
tuyul ilang <mahdysyafiq160998@gmail.com>
2022-05-09 09:24:13 +0000

    Menambahkan materi dan latihan

---
+++ 1-intro/2-variable/variable.swift
@@ -0,0 +1,14 @@
+// Pembuatan variabel di Swift
+// Variable yg udah dideklarasi gabisa diubah tipe datanya
+var num = 10;
+var numDeclare:Int = 5;
+
+print(num);
+print(numDeclare);
+
+// Constant di Swift
+let name = "Anton";
+let nameDeclare:String = "Ando";
+
+print(name);
+print(nameDeclare);
